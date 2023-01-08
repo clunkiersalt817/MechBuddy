e3c43173217db30fa594bff7a1f706f18e5afccb
clunkiersalt817 <clunkierontv@gmail.com>
2023-01-08 21:55:41 +0530

    added fragments v1.0.3

--- app/src/main/java/com/mechbuddy/app/HomeActivity.kt
+++ app/src/main/java/com/mechbuddy/app/HomeActivity.kt
@@ -2,12 +2,38 @@ package com.mechbuddy.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
 import com.mechbuddy.app.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = android.widget.PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title=when(destination.id){
+                    R.id.cartFragment -> "My Cart"
+                    R.id.homeFragment -> "Home"
+                    else -> "Profile"
+                }
+            }
+        })
     }
 }
