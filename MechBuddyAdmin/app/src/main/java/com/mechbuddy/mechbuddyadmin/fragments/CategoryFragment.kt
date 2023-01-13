74dedd9d41e531aec219c62bf41eb53ceb3dc5c1
clunkiersalt817 <clunkierontv@gmail.com>
2023-01-14 02:03:50 +0530

    product category

--- MechBuddyAdmin/app/src/main/java/com/mechbuddy/mechbuddyadmin/fragments/CategoryFragment.kt
+++ MechBuddyAdmin/app/src/main/java/com/mechbuddy/mechbuddyadmin/fragments/CategoryFragment.kt
@@ -1,32 +1,33 @@
 package com.mechbuddy.mechbuddyadmin.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.res.ResourcesCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.mechbuddy.mechbuddyadmin.R
+import com.mechbuddy.mechbuddyadmin.databinding.FragmentCategoryBinding
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding : FragmentCategoryBinding
+    private var imageUrl : Uri? = null
+
+    private var launchGalleryActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == Activity.RESULT_OK){
+            imageUrl = it.data!!.data
+            binding.imageView1.setImageURI(imageUrl)
         }
     }
 
@@ -35,26 +36,61 @@ class CategoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        binding = FragmentCategoryBinding.inflate(layoutInflater)
+
+        binding.imageView1.setOnClickListener {
+            val intent = Intent("android.intent.action.GET_CONTENT")
+            intent.type="image/*"
+            launchGalleryActivity.launch(intent)
+        }
+        binding.button1.setOnClickListener {
+            validateData(binding.categoryName.text.toString())
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun validateData(categoryName:String) {
+        if(categoryName.isEmpty()){
+            Toast.makeText(requireContext(), "Category Name cannot be empty !", Toast.LENGTH_SHORT).show()
+        }
+        else if (imageUrl==null){
+            Toast.makeText(requireContext(), "Please select image !", Toast.LENGTH_SHORT).show()
+        }
+        else
+            uploadImage(categoryName)
+    }
+
+    private fun uploadImage(categoryName: String) {
+        val filename = UUID.randomUUID().toString()+".jpg"
+        val imageRef = FirebaseStorage.getInstance().reference.child("category/$filename")
+        imageRef.putFile(imageUrl!!)
+            .addOnSuccessListener {
+                it.storage.downloadUrl
+                    .addOnSuccessListener{
+                        downloadImageUrl ->
+                        storeData(categoryName,downloadImageUrl.toString())
+                    }
+            }
+            .addOnFailureListener{
+                Toast.makeText(requireContext(), "Something went wrong with storage", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun storeData(categoryName: String, url: String) {
+        val db = Firebase.firestore
+        val data = hashMapOf<String,Any>(
+            "cat" to categoryName,
+            "img" to url
+        )
+        db.collection("Categories").add(data)
+            .addOnSuccessListener {
+                binding.imageView1.setImageDrawable(ResourcesCompat.getDrawable(requireContext().resources,R.drawable.upload,requireContext().theme))
+                binding.categoryName.text=null
+                Toast.makeText(requireContext(), "Category Added", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(requireContext(), "Something went wrong with Storage !", Toast.LENGTH_SHORT).show()
             }
     }
 }
